ac89094ee121eac3add7b3b8ecd54b4823890677
Henning Sprang <henning.sprang@gmail.com>
2018-04-12 00:07:15 +0200

    adjust port

--- docker-run.sh
+++ docker-run.sh
@@ -4,11 +4,9 @@ VENV_PYTHON="../venv/bin/python"
 
 MANAGE="$VENV_PYTHON manage.py"
 
-#cd $(dirname $0)
-
 if [ -z $1 ] || [ $1 = 'run' ]; then
     $MANAGE migrate
-    $MANAGE runserver 0.0.0.0:8000
+    $MANAGE runserver 0.0.0.0:9000
 elif [ $1 = 'dumpdata' ]; then
     $MANAGE dumpdata
 else
