8f5a1b3577e1309bcb2c148a750134fbb983a9bd
Henning Sprang <henning.sprang@gmail.com>
2018-04-12 00:07:35 +0200

    run prod in docker

--- run-production.sh
+++ run-production.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
-
-set -x
-
-source /etc/bash_completion
-
-
-cd $(dirname $0)
-
-workon freelance-organizer-production
-
-set -e
-
-pip install -r requirements.txt
-
-#cp db.sqlite3 ~/tmp/backups/db.sqlite3_$(date +%Y-%m-%d_%s)
-source db-tmp-backup.sh
-
-./manage.py runserver 8888 2>&1 >>production.log
+docker run -it \
+  --name freelance-organizer \
+  -v $(pwd):/var/lib/freelance-organizer \
+  -p 9000:9000 \
+  freelance-organizer:production
