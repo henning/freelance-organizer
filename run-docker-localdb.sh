d194a2b334d1469ac2fe21f868af4064ebe41d19
Henning Sprang <henning.sprang@gmail.com>
2022-11-04 15:03:13 +0100

    Add restart yes only for prod run

--- run-docker-localdb.sh
+++ run-docker-localdb.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 docker run -d \
-	--restart \
   --name freelance-organizer \
   -v $(pwd):/var/lib/freelance-organizer \
   -p 9000:9000 \
--- run-production.sh
+++ run-production.sh
@@ -4,6 +4,7 @@
 DB_DIR=/home/henning/data/produktion/freelance-organizer/prod-db
 
 docker run -d \
+  --restart yes \
   --name freelance-organizer \
   -v $DB_DIR:/var/lib/freelance-organizer \
   -p 9000:9000 \
