15cfe69df47e8aade9a2d4be7ac8c53999c09307
Henning Sprang <henning.sprang@gmail.com>
2023-07-09 19:41:39 +0200

    Update docker build and run
    * adjust dockerfile to poetry usage
    * rename and adjust start/entrypoint script

--- docker/run-app.sh
+++ docker/run-app.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-VENV_PYTHON="../venv/bin/python"
+VENV_BIN="./venv/bin/"
 
-MANAGE="$VENV_PYTHON manage.py"
+MANAGE="${VENV_BIN}/django-admin"
 
 if [ -z $1 ] || [ $1 = 'run' ]; then
     $MANAGE migrate
     $MANAGE runserver 0.0.0.0:9000
 elif [ $1 = 'dumpdata' ]; then
     $MANAGE dumpdata
+elif [ $1 = 'shell' ]; then
+    /bin/bash
 else
     echo "unknown command / parameters $@"
     exit 1
